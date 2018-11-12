43f1481349d13b422a56caec0db78881d659d8de
Jason Jean <jasonjean1993@gmail.com>
2018-11-11 21:14:52 -0500

    Added Application

---
+++ apps/boardgame-website/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'boardgame-website',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/boardgame-website/'
+};
---
+++ apps/boardgame-website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'boardgame-website-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'boardgame-website';
+}
