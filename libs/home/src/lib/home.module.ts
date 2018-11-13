117fb68b40b07afb95f6cf9c840777ebeaa61655
Taylor Tu <tkt7@njit.edu>
2018-11-12 23:16:05 -0500

    install angular material and created homepage component

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { HomepageComponent } from './homepage/homepage.component';
+import { MatButtonModule } from '@angular/material';
+
 @NgModule({
+  declarations: [
+    HomepageComponent,
+  ],
   imports: [
     CommonModule,
+    MatButtonModule,
 
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      {path: '', pathMatch: 'full', component: HomepageComponent}
     ])
   ]
 })
